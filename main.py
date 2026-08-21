316022ce86f4de82a628cfa500782943ad990ece
lelero17 <leandro-emmerling@web.de>
2026-08-21 18:13:25 +0200

    feat: run simulation and print turn-by-turn output

--- main.py
+++ main.py
@@ -4,14 +4,21 @@
 from parser import Parser
 from error import ParserError, MapValidationError, PathNotFoundError
 from pathfinder import Pathfinder
+from simulation import Simulation
 
 
 if __name__ == "__main__":
-    p = Parser()
+    turn_count = 0
     try:
+        p = Parser()
         game_map = p.parse("config.txt")
         pf = Pathfinder(game_map)
+        sim = Simulation(game_map, pf)
         path = pf.find_path(game_map.start, game_map.end)
+        for turn in sim.run():
+            turn_count += 1
+            print(f"Turn {turn_count}: ", end="")
+            print(*turn)
     except (ParserError, MapValidationError) as e:
         print(f"Error: {e}")
         exit(1)
